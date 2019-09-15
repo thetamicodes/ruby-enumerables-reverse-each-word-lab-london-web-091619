96eb33b79d4fb89dc4e34ef8e07cf3b91ed322c6
Tami Reichold <reichold.tamara@gmail.com>
2019-09-15 14:07:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -14,8 +14,9 @@ a.collect { |x| x + "!" }         #=> ["a!", "b!", "c!", "d!"]
 def reverse_each_word(string)
 array = string.split
 reversed_words = []
-array.collect { |word| reversed_words << word.reverse }
-puts reserved_words
+  array.collect { |word| reversed_words << word.reverse }
+  puts reserved_words
+  end
 # binding.pry
 end
     
