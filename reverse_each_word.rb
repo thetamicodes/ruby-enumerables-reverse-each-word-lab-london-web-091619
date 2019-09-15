6928fb0bf161e2c63d3b41cd3adc2e67e0936d8e
Tami Reichold <reichold.tamara@gmail.com>
2019-09-15 13:26:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,5 @@
 def reverse_each_word(string)
-  new_array = []
-  sentence = string
-  new_array << sentence
-  new_array.each do |word|
-    word.reverse 
-  end
-  return new_array
+  
 end
 
     
