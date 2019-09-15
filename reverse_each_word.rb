29dc8a19d371dd1ec1afcc1401077b7d1ac91268
Tami Reichold <reichold.tamara@gmail.com>
2019-09-15 14:43:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -12,11 +12,17 @@ require 'pry'
 
 def reverse_each_word(string)
 array = string.split
-reversed_words = []
-array.collect { |word| reversed_words << word.reverse }
-binding.pry
-return
+  reversed_sentence = array.collect do |words|
+    p words.reverse 
+      reverse.join(" ")
+  end
 end
+
+# def reverse_each_word(string)
+# newString = string.split
+# reversed = newString.collect do |words| p words.reverse end
+# reversed.join(" ")
+# end
     
 
 
