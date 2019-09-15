6258279f617e23df02637cc2c86ae101a4a042df
Tami Reichold <reichold.tamara@gmail.com>
2019-09-15 14:43:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -14,7 +14,7 @@ def reverse_each_word(string)
 array = string.split
   reversed_sentence = array.collect do |words|
     p words.reverse 
-      reverse.join(" ")
+    reverse.join(" ")
   end
 end
 
