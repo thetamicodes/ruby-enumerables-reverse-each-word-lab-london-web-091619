58ffd6fe0b4e8a188192c31e2ee1fb2bb82ea729
Tami Reichold <reichold.tamara@gmail.com>
2019-09-15 14:32:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -14,7 +14,7 @@ def reverse_each_word(string)
 array = string.split
 reversed_words = []
 array.collect { |word| reversed_words << word.reverse }
-# reversed_words.map { |word| "" + word.to_s + "" }.join(" ") }
+reversed_words.map { |word| "" + word.to_s + "" }.join(" ") }
 binding.pry
 return
 end
