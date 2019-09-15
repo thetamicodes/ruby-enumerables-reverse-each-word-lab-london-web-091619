044dd9a419d23fe51a21c9e64f090a23ece64bc2
Tami Reichold <reichold.tamara@gmail.com>
2019-09-15 14:48:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,26 +1,11 @@
 require 'pry'
 
-# def reverse_each_word(string)
-# array = string.split
-# reversed_words = []
-# array.each { |word| reversed_words << word.reverse }
-# puts reversed_words 
-# reversed_words.collect { |words| puts words }
-# # binding.pry
-# end
-
-
 def reverse_each_word(string)
 array = string.split
 reversed_sentence = array.collect { |words| p words.reverse }
 reversed_sentence.join(" ")
 end
 
-# def reverse_each_word(string)
-# newString = string.split
-# reversed = newString.collect do |words| p words.reverse end
-# reversed.join(" ")
-# end
     
 
 
