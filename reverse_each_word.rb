805f748a90059f5905f7eb235ed01b2babba26ce
Tami Reichold <reichold.tamara@gmail.com>
2019-09-15 14:48:24 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,15 @@
-def reverse_each_word(String)
-  newArray = %w(String).reverse
-  newArray.each {|i| puts i}
-  return newArray
+require 'pry'
+
+def reverse_each_word(string)
+array = string.split
+reversed_sentence = array.collect { |words| p words.reverse }
+reversed_sentence.join(" ")
 end
+
+    
+
+
+
+
+
+
